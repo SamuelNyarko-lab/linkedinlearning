e49aa53b94fa9cc8749bcbe0df7825af0d081fd3
Samuel Nyarko <samuelnyarko003@gmail.com>
2024-10-04 20:22:37 +0000

    updating model

---
+++ lib/models/author.dart
@@ -0,0 +1,4 @@
+class Author {
+  final String id;
+  Author({required this.id});
+}
--- lib/models/chat_entity.dart
+++ lib/models/chat_entity.dart
@@ -1,5 +1,10 @@
+import 'package:linkedinlearning/models/author.dart';
+
 class ChatEntity {
   final String username;
+  final String id;
+  final int timeStamp;
+  Author author;
 
-  ChatEntity({required this.username});
+  ChatEntity({required this.id, required this.timeStamp, required this.username, required this.author});
 }
